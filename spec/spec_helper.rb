0564d881d0690015836262f0783b0a324b4c4b44
Galih Muhammad <galih0muhammad@gmail.com>
2017-03-25 23:30:32 +0000

    add spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,29 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "xendit_api"
+require 'webmock/rspec'
+require 'capybara/rspec'
+WebMock.disable_net_connect!
+
+RSpec.configure do |config|
+	config.treat_symbols_as_metadata_keys_with_true_values = true 
+	config.run_all_when_everything_filtered = true
+	config.filter_run :focus 
+  config.order = 'random' 
+
+  # Stubs
+  # config.before(:each) do
+  #   stub_request(:get, 'https://sheetsu.com/apis/foo')
+  #     .with(
+  #       headers: {
+  #         'Accept' => '*/*',
+  #         'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+  #         'User-Agent' => 'Ruby'
+  #       }
+  #      )
+  #     .to_return(
+  #       status: 200,
+  #       body: '{"status": 200, "success": true, "result": []}',
+  #       headers: {}
+  #      )
+  end  
+end
